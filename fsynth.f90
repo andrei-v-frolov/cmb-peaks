84477b7e6f1ca6735c54eeca480fa902762a6dab
Andrei Frolov <frolov@sfu.ca>
2012-05-26 22:41:27 +0000

    Minkowski functional estimators implemented.

--- fsynth.f90
+++ fsynth.f90
@@ -1,6 +1,8 @@
 ! $Id$
-! Synthesize a filtered map, applying a mask correction, and output hot pixels
-! invoke: wiener ... | fsynth <mmap-alms.fits> <map.fits[:iqu]> [mask-alms.fits[:tdb]]
+! Synthesize a filtered map, applying a mask correction, and output map statistics
+! invoke: wiener ... | fsynth <mmap-alms.fits> <map.fits[:mode]> [mask-alms.fits[:tdb]]
+!   - mode :iqu synthesizes IQU decomposition map of tidal tensor, outputs hot pixels
+!   - mode :inv synthesizes curvature invariants map, outputs Minkowski functionals
 
 program fsynth
 
@@ -15,6 +17,8 @@ use pix_tools
 implicit none
 
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+integer, parameter :: default = 0, iqu = 1, inv = 2; integer :: mode = default
 
 integer, parameter :: lmax = 1535, nside = 512, hmax = 256
 character(len=80) :: header(hmax), fmap, fmask, fout
@@ -37,8 +41,9 @@ end do
 call getArgument(1, fmap)
 call getArgument(2, fout)
 
-i = index(fout, ":iqu", .true.); if (i > 0) fout(i:) = ""
-call make_map(fmap, beam, lmax, nside, Mmap, n, nmaps, (i > 0))
+i = index(fout, ":iqu", .true.); if (i > 0) then; fout(i:) = ""; mode = iqu; end if
+i = index(fout, ":inv", .true.); if (i > 0) then; fout(i:) = ""; mode = inv; end if
+call make_map(fmap, beam, lmax, nside, Mmap, n, nmaps, mode)
 
 ! make filtered mask
 if (nArguments() < 3) then
@@ -47,7 +52,7 @@ else
 	call getArgument(3, fmask); i = index(fmask, ":", .true.)
 	if (i > 0) then; read (fmask(i+1:),*) tol; fmask(i:) = ""; end if
 	
-	call make_map(fmask, beam/beam(0), lmax, nside, Mask, n, mmaps, .false.)
+	call make_map(fmask, beam/beam(0), lmax, nside, Mask, n, mmaps, default)
 	
 	! remove non-compliant pixels
 	where (Mask <= 0.0 .or. abs(log10(Mask)) > tol/10.0) Mask = 0.0
@@ -55,33 +60,35 @@ end if
 
 ! mask correction
 allocate (Mout(0:n,nmaps))
-do i = 0,n
-	Mout(i,:) = Mmap(i,:)/Mask(i,1)
-end do
+forall (i = 0:n) Mout(i,:) = Mmap(i,:)/Mask(i,1)
 
-! output extrema distribution in first extent
-call extrema(Mout(:,1), Mask(:,1), nside, n)
+! output map statistics
+select case (mode)
+	case (default,iqu); call extrema(Mout(:,1), Mask(:,1), nside, n)	! extrema distribution
+	case (inv);         call minkowski(Mout, Mask(:,1), nmaps, n, 1024)	! cumulative Minkowski functionals
+end select
 
 ! output corrected map
-call write_minimal_header(header, 'MAP', nside=nside, order=2, creator='FSYNTH', version='$Revision$', polar=(nmaps .eq. 3))
+call write_minimal_header(header, 'MAP', nside=nside, order=2, creator='FSYNTH', version='$Revision$', polar=(mode .eq. iqu))
 call output_map(Mout, header, '!'//fout)
 
 
 contains
 
 ! synthesize a map from alms convolved with a beam function
-subroutine make_map(falms, bls, lmax, nside, map, n, nout, decompose)
+subroutine make_map(falms, bls, lmax, nside, map, n, nout, mode)
 	character(*) falms; real bls(0:lmax)
-	integer lmax, nalms, nside, next, nout, j, k, l, m, n
-	logical, optional :: decompose; logical iqu
+	integer lmax, nalms, nside, next, nout, mode, j, k, l, m, n
 	character(len=80) :: header(hmax,3)
 	real(DP), allocatable :: aks(:,:,:), map(:,:)
 	complex(DPC), allocatable :: alms(:,:,:)
 	
 	! read in alms data
-	iqu = present(decompose) .and. decompose
 	nalms = number_of_alms(falms, next); n = nside2npix(nside)-1
-	if (iqu) then; next = 1; nout = 3; else; nout = next; end if
+	select case (mode)
+		case (iqu,inv); next = 1; nout = 3
+		case default; nout = next
+	end select
 	allocate(aks(nalms,4,next), alms(next,0:lmax,0:lmax), map(0:n,nout))
 	
 	call fits2alms(falms, nalms, aks, 3, header, hmax, next)
@@ -94,15 +101,55 @@ subroutine make_map(falms, bls, lmax, nside, map, n, nout, decompose)
 	end do; end do
 	
 	! synthesize map (in nested ordering)
-	if (iqu) then
-		call alm2map_iqu(nside, lmax, lmax, alms, map(:,1:nout))
-	else
-		if (next == 1) call alm2map(nside, lmax, lmax, alms, map(:,1))
-		if (next >  1) call alm2map(nside, lmax, lmax, alms, map(:,1:next))
-	end if
+	select case (mode)
+		case (iqu); call alm2map_iqu(nside, lmax, lmax, alms, map(:,1:nout))
+		case (inv); call alm2map_inv(nside, lmax, lmax, alms, map(:,1:nout))
+		case default;
+			if (next == 1) call alm2map(nside, lmax, lmax, alms, map(:,1))
+			if (next >  1) call alm2map(nside, lmax, lmax, alms, map(:,1:next))
+	end select
 	call convert_ring2nest(nside, map)
 	
 	deallocate(aks, alms)
+end subroutine
+
+! synthesize curvature invariants map for Minkowski functional evaluation (using canned HEALPix derivatives)
+subroutine alm2map_inv(nside, lmax, mmax, alms, Minv)
+	integer p, l, m, n, nside, lmax, mmax
+	complex(DPC) alms(1,0:lmax,0:mmax)
+	real(DP) Minv(12*nside**2,3), theta, phi
+	
+	! temporary storage
+	real(DP), allocatable :: I(:), C(:), D1(:,:), D2(:,:)
+	
+	n = nside2npix(nside)-1
+	allocate(I(0:n), C(0:n), D1(0:n,2), D2(0:n,3))
+	
+	! synthesize component maps from alm's
+	call alm2map_der(nside, lmax, mmax, alms, I, D1, D2)
+	
+	do p = 0,n
+		! cot(theta) map (in ring ordering)
+		call pix2ang_ring(nside, p, theta, phi); C(p) = cotan(theta)
+		
+		! second covariant dervatives
+		D2(p,2) = D2(p,2) - C(p) * D1(p,2)
+		D2(p,3) = D2(p,3) + C(p) * D1(p,1)
+		
+		! covariant gradient squared
+		C(p) = D1(p,1)**2 + D1(p,2)**2
+		
+		! Minkowski functional maps
+		Minv(p,1) = I(p)
+		Minv(p,2) = sqrt(C(p))
+		Minv(p,3) = - (D2(p,1)*D1(p,2)**2 - 2.0*D2(p,2)*D1(p,1)*D1(p,2) + D2(p,3)*D1(p,1)**2)/C(p)
+		
+		! skeleton map
+		!Minv(p,4) = (D2(p,3) - D2(p,1))*D1(p,1)*D1(p,2) + D2(p,2) * (D1(p,1)**2 - D1(p,2)**2)
+	end do
+	
+	! clean up after ourselves
+	deallocate(I, C, D1, D2)
 end subroutine
 
 ! synthesize an IQU decomposition map of tidal tensor (using canned HEALPix derivatives)
@@ -206,6 +253,35 @@ subroutine extrema(M, mask, nside, n)
 		if (M(i) > maxval(M(nn(1:k)))) write (*,'(I,G24.16,I)') i, M(i), +1
 		if (M(i) < minval(M(nn(1:k)))) write (*,'(I,G24.16,I)') i, M(i), -1
 	end do
+end subroutine
+
+! calculate cumulative Minkowski functionals
+subroutine minkowski(M, mask, nmaps, n, bins)
+	integer i, k, n, p, nmaps, bins, s, o
+	real(DP) F(nmaps), M(0:n,nmaps), mask(0:n)
+	real(SP), allocatable :: U(:)
+	integer, allocatable :: idx(:), pxl(:)
+	
+	! temporary storage
+	allocate(U(n+1), idx(n+1), pxl(n+1))
+	
+	! index unmasked pixels
+	k = 0; do i = 0,n; if (mask(i) /= 0.0) then
+		k = k+1; U(k) = M(i,1); pxl(k) = i
+	end if; end do
+	
+	call indexx(k, U, idx)
+	
+	! output accumulated distribution
+	F = 0.0; s = k/bins; o = (k - bins*s)/2 + 1
+	
+	do i = 1,k; p = pxl(idx(i))
+		F = F + (/ 1.0, M(p,2:) /)
+		if (mod(i-o,s) == 0) write (*,'(8G24.16)') M(p,1), F/k
+	end do
+	
+	! clean up after ourselves
+	deallocate(U, idx, pxl)
 end subroutine
 
 end

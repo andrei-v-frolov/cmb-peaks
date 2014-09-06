8c86842b5231b8e9b302d139c9de1a47c073b017
Andrei Frolov <frolov@sfu.ca>
2014-09-05 20:56:48 -0700

    Clean up GIF generation code with loops...

--- xtra/paramap.sh
+++ xtra/paramap.sh
@@ -21,13 +21,12 @@ awk '{print $1,$2,$7;}' LOCAL-PARAM | $BINDIR/pxl2map order-nest.fits peaks-alph
 awk '{print $1,$2,$8;}' LOCAL-PARAM | $BINDIR/pxl2map order-nest.fits peaks-local.fits
 
 # and the corresponding GIFs
-map2gif -xsz 800 -bar .true. -inp peaks-n.fits -out '!peaks-n.gif'
-map2gif -xsz 800 -bar .true. -inp peaks-ksdev.fits -out '!peaks-ksdev.gif'
-map2gif -xsz 800 -bar .true. -min 0.5137003373 -inp peaks-ksdev.fits -out '!peaks-ksdev-95.gif'
-map2gif -xsz 800 -bar .true. -min 0.7170542898 -inp peaks-ksdev.fits -out '!peaks-ksdev-99.gif'
-map2gif -xsz 800 -bar .true. -inp peaks-gamma.fits -out '!peaks-gamma.gif'
-map2gif -xsz 800 -bar .true. -inp peaks-sigma.fits -out '!peaks-sigma.gif'
-map2gif -xsz 800 -bar .true. -inp peaks-alpha.fits -out '!peaks-alpha.gif'
-map2gif -xsz 800 -bar .true. -inp peaks-local.fits -out '!peaks-local.gif'
-map2gif -xsz 800 -bar .true. -min 0.5137003373 -inp peaks-local.fits -out '!peaks-local-95.gif'
-map2gif -xsz 800 -bar .true. -min 0.7170542898 -inp peaks-local.fits -out '!peaks-local-99.gif'
+for i in n ksdev gamma sigma alpha local; do
+	map2gif -xsz 800 -bar .true. -inp peaks-$i.fits -out "!peaks-$i.gif"
+done
+
+# and the thresholded GIFs
+for i in ksdev local; do
+	map2gif -xsz 800 -bar .true. -min 0.5137003373 -inp peaks-$i.fits -out "!peaks-$i-95.gif"
+	map2gif -xsz 800 -bar .true. -min 0.7170542898 -inp peaks-$i.fits -out "!peaks-$i-99.gif"
+done

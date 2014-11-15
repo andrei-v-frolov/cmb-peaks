d3144ed0fd9298b4e3678270b9f7420b53cf467e
Andrei Frolov <frolov@sfu.ca>
2014-11-15 13:21:02 -0800

    Sweep range adjusted...

--- sweep.sh
+++ sweep.sh
@@ -7,18 +7,20 @@ set -u -e -o errexit
 BINDIR=./bins/`uname -m`-`uname -s`
 
 # base kernel to sweep
-KERNEL='L1-SSG84'
+KERNEL='SSG84'
 
 # sims scaling factor
 FUDGE=1.0134
 
 # kernel size sweep
-for i in {120..999}; do
-	make stat/$KERNEL-$i.dat; rm -f maps/$KERNEL-$i.fits
-	python stat/normalize.py stat/$KERNEL-$i.dat > stat/$KERNEL-$i.sig
-	#python stat/normalize.py stat/$KERNEL-$i.dat:mcmc/peakcdf/$KERNEL-$i.dat:$FUDGE > stat/$KERNEL-$i.sig
+for i in {40..600}; do
+	BASE=`printf "L1-$KERNEL-%03i" $i`
+	make stat/$BASE.dat gifs/$BASE.gif; rm -f maps/$BASE.fits
+	python stat/normalize.py stat/$BASE.dat > stat/$BASE.sig
+	#python stat/normalize.py stat/$BASE.dat:mcmc/peakcdf/$BASE.dat:$FUDGE > stat/$BASE.sig
 done
 
 # make a merger map
-cat stat/$KERNEL-*.sig | $BINDIR/pxl2map maps/white.fits maps/merger.fits
+cat stat/L1-$KERNEL-*.sig | $BINDIR/pxl2map maps/white.fits maps/merger.fits
 map2gif -inp maps/merger.fits -out '!gifs/merger.gif' -xsz 2400
+map2gif -inp maps/merger.fits -out '!gifs/merger-zoom.gif' -pro GNO -lat -56.73 -lon 209.96 -res 3.3 -xsz 1200 -bar true

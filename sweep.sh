a3c72ac15c414220474cd4231cd8cf3a972d0445
Andrei Frolov <frolov@sfu.ca>
2014-11-14 19:06:44 -0800

    Full sweep range enabled, parallel execution is not needed (fsynth is parallelized anyway)...

--- sweep.sh
+++ sweep.sh
@@ -12,9 +12,12 @@ KERNEL='L1-SSG84'
 # sims scaling factor
 FUDGE=1.0134
 
-for i in {120..120}; do
-	echo "make stat/$KERNEL-$i.dat; rm -f maps/$KERNEL-$i.fits; python stat/normalize.py stat/$KERNEL-$i.dat:mcmc/peakcdf/$KERNEL-$i.dat:$FUDGE > stat/$KERNEL-$i.sig"
-done | parallel
+# kernel size sweep
+for i in {120..999}; do
+	make stat/$KERNEL-$i.dat; rm -f maps/$KERNEL-$i.fits
+	python stat/normalize.py stat/$KERNEL-$i.dat:mcmc/peakcdf/$KERNEL-$i.dat:$FUDGE > stat/$KERNEL-$i.sig
+done
 
+# make a merger map
 cat stat/$KERNEL-*.sig | $BINDIR/pxl2map maps/white.fits maps/merger.fits
 map2gif -inp maps/merger.fits -out '!gifs/merger.gif' -xsz 2400

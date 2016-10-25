1e60321ffcfddab996ea99e3789381c1439d8c53
Andrei Frolov <frolov@sfu.ca>
2016-10-25 12:56:25 -0700

    udgrade_nest on complex maps implemented.

--- complex-qu.f90
+++ complex-qu.f90
@@ -6,15 +6,18 @@ use, intrinsic :: ISO_C_BINDING
 ! HEALPix modules
 use healpix_types
 use pix_tools
+use udgrade_nr
 
 implicit none
 
-public :: convert_ring2nest
+public :: convert_ring2nest, convert_nest2ring, udgrade_nest
 
 ! generic iterfaces are implemented using Fortran preprocessor
 #define GENERIC(name) interface name; module procedure name ## _zs, name ## _zd; end interface
 
 GENERIC(convert_ring2nest)
+GENERIC(convert_nest2ring)
+GENERIC(udgrade_nest)
 
 contains
 

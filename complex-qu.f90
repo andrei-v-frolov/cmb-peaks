c609f1a01efe4b4116c5fa3b36e2e794dc3767ea
Andrei Frolov <frolov@sfu.ca>
2016-12-15 19:09:43 -0800

    First try at pure alm estimators from apodized mask boundary terms…

--- complex-qu.f90
+++ complex-qu.f90
@@ -11,6 +11,17 @@ use udgrade_nr
 implicit none
 
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+! HEALPix routine wrappers, covariant derivatives, in single and double precision
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+! generic iterfaces are implemented using Fortran preprocessor
+#define GENERIC(name) interface name; module procedure name ## _sp, name ## _dp; end interface
+
+GENERIC(alm2map_covariant)
+
+public :: alm2map_covariant
+
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 ! HEALPix routine wrappers, complex QU, in single and double precision
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
@@ -30,13 +41,14 @@ public :: convert_ring2nest, convert_nest2ring, udgrade_nest
 ! generic iterfaces are implemented using Fortran preprocessor
 #define GENERIC(name) interface name; module procedure name ## _sp, name ## _dp, name ## _zs, name ## _zd; end interface
 
+GENERIC(map2alm_pure)
 GENERIC(rotate_qu2eb)
 GENERIC(rotate_eb2qu)
 GENERIC(project_qu)
 GENERIC(krylov_qu)
 GENERIC(purify_qu)
 
-public :: rotate_qu2eb, rotate_eb2qu, project_qu, krylov_qu, purify_qu
+public :: map2alm_pure, rotate_qu2eb, rotate_eb2qu, project_qu, krylov_qu, purify_qu
 
 contains
 
@@ -51,11 +63,13 @@ contains
 #define XP SP
 #define VARIANT(name) name ## _sp
 #include 'almtools.fin'
+#include 'covariant.fin'
 
 ! double precision
 #define XP DP
 #define VARIANT(name) name ## _dp
 #include 'almtools.fin'
+#include 'covariant.fin'
 
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
